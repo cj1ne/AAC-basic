7957d2a418f944b4fef0c95873d5c0dfd2123814
glen.jw <glen.jw@kakaocorp.com>
2021-10-11 16:17:27 +0900

    init commit

---
+++ app/src/main/java/com/glen/aacbasic/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.glen.aacbasic.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.glen.aacbasic.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
